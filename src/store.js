2c5fc812b7a451d9f4331862b21580c42bcae91e
Damian Sandhu-Franceschi <damiansandhu-franceschi@Damians-MacBook-Pro.local>
2019-07-01 22:38:50 -0400

    adding add user feature

--- src/store.js
+++ src/store.js
@@ -2,6 +2,6 @@ import {createStore} from 'redux';
 import reducer from './reducers/reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools());
 
 export default store;
