f7cc71c7ed7fabafaf79ea28b3d2df016b122edd
damiansf <damiansandhu11@gmail.com>
2019-08-28 23:38:17 -0400

    fixing gh pages issue

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import AddUserContainer from "./containers/addUserContainer";
 import AddDebtContainer from "./containers/addDebtContainer";
 import DisplayDebtsContainer from "./containers/displayDebtsContainer";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter, Route, Link } from "react-router-dom";
 
 export default class App extends React.Component {
   render() {
@@ -11,16 +11,27 @@ export default class App extends React.Component {
         <header>
           <p>How Much Do I Owe You?</p>
         </header>
-        <Router>
-          <Route path="/addUser" component={AddUserContainer} />
-          <Route path="/addDebt" component={AddDebtContainer} />
-          <Route path="/displayDebts" component={DisplayDebtsContainer} />
-        </Router>
-        <a href="/addUser">Add User</a>
-        <br></br>
-        <a href="/addDebt">Add Debt</a>
-        <br></br>
-        <a href="/displayDebts">Display Debts</a>
+        <HashRouter basename="/">
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/addUser">Add User</Link>
+              </li>
+              <li>
+                <Link to="/addDebt">Add Debt</Link>
+              </li>
+              <li>
+                <Link to="/displayDebts">Display Debts</Link>
+              </li>
+            </ul>
+            <Route path="/addUser" component={AddUserContainer} />
+            <Route path="/addDebt" component={AddDebtContainer} />
+            <Route path="/displayDebts" component={DisplayDebtsContainer} />
+          </div>
+        </HashRouter>
       </div>
     );
   }
