3495dd20350ce8df5b1d5460224404b04f6e5577
damiansf <damiansandhu11@gmail.com>
2019-08-29 22:10:22 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HowMuchDoIOweYou/precache-manifest.d1176e8ff7858f29fe1df62e5112119d.js"
+  "/HowMuchDoIOweYou/precache-manifest.d7c5370e9d3e0880c33d9477879709c4.js"
 );
 
 self.addEventListener('message', (event) => {
