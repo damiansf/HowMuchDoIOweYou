2c523c4d299ffc679b865d100e085c4f193dcfae
damiansf <damiansandhu11@gmail.com>
2019-09-03 23:04:27 -0400

    Updates

---
+++ precache-manifest.32697c29dbf98fec81024ac963a4b369.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76db689b89d8650d199d8ab89b9465cc",
+    "url": "/HowMuchDoIOweYou/index.html"
+  },
+  {
+    "revision": "c084d6e3be3d136193f0",
+    "url": "/HowMuchDoIOweYou/static/css/main.3a695f12.chunk.css"
+  },
+  {
+    "revision": "534b8f9b61910045a845",
+    "url": "/HowMuchDoIOweYou/static/js/2.4125fd9a.chunk.js"
+  },
+  {
+    "revision": "c084d6e3be3d136193f0",
+    "url": "/HowMuchDoIOweYou/static/js/main.b36057c0.chunk.js"
+  },
+  {
+    "revision": "26382ecdc9a2839f2bbc",
+    "url": "/HowMuchDoIOweYou/static/js/runtime~main.a16422a2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HowMuchDoIOweYou/precache-manifest.30c063fc4443e99fbadfa6cf1b869a57.js"
+  "/HowMuchDoIOweYou/precache-manifest.32697c29dbf98fec81024ac963a4b369.js"
 );
 
 self.addEventListener('message', (event) => {
